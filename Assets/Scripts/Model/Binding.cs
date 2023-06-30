c47cd3ab26b5b49d65d37b8f15f7a57425725734
zugamifk <zugamifk@gmail.com>
2023-06-30 17:44:00 -0300

    add bindings

---
+++ Assets/Scripts/Model/Binding.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Binding<T> : IBinding<T>
+{
+    public event Action<T, T> ValueChanged;
+
+    private T value;
+    public T Value
+    {
+        get => value;
+        set
+        {
+            if (!this.value.Equals(value))
+            {
+                T oldValue = this.value;
+                this.value = value;
+                ValueChanged?.Invoke(oldValue, value);
+            }
+        }
+    }
+}
